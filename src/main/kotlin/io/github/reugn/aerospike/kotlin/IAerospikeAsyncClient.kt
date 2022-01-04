67fac41dd244f64859c1e9215c30b845ee61da18
reugn <reugpro@gmail.com>
2022-01-04 17:12:03 +0200

    change delegate client type to interface

--- src/main/kotlin/io/github/reugn/aerospike/kotlin/IAerospikeAsyncClient.kt
+++ src/main/kotlin/io/github/reugn/aerospike/kotlin/IAerospikeAsyncClient.kt
@@ -11,7 +11,7 @@ import java.util.*
 
 interface IAerospikeAsyncClient : Closeable {
 
-    fun asJava(): AerospikeClient
+    fun asJava(): IAerospikeClient
 
     //-------------------------------------------------------
     // Write Record Operations
